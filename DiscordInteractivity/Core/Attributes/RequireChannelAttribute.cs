a7bc06ebd6ef32bb584eb1938d131f01bb3cf95a
Twenty <twenty@translucent.at>
2019-03-28 14:50:57 +0100

    Added useful Preconditions

---
+++ DiscordInteractivity/Core/Attributes/RequireChannelAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordInteractivity.Core.Attributes
+{
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+	public class RequireChannelAttribute : PreconditionAttribute
+	{
+		private readonly ulong[] _channelIds;
+
+		public RequireChannelAttribute(params ulong[] channelIds)
+		{
+			_channelIds = channelIds;
+		}
+
+		public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+		{
+			if (!_channelIds.Any(x => x == context.Channel.Id))
+			{
+				return Task.FromResult(PreconditionResult.FromError("Command executed in wrong channel."));
+			}
+			return Task.FromResult	(PreconditionResult.FromSuccess());
+		}
+	}
+}
---
+++ DiscordInteractivity/Core/Attributes/RequireRoleAttribute.cs
@@ -0,0 +1,39 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordInteractivity.Core.Attributes
+{
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+	public class RequireRoleAttribute : PreconditionAttribute
+	{
+		private readonly ulong[] _roleIds;
+
+		public bool MatchAllRoles = true;
+
+		public RequireRoleAttribute(params ulong[] roleIds)
+		{
+			_roleIds = roleIds;
+		}
+
+		public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+		{
+			if (!(context.User is SocketGuildUser user))
+				return Task.FromResult(PreconditionResult.FromError("Command not invoked in a Guild."));
+
+			if (!MatchAllRoles && _roleIds.Any(x => user.Roles.Any(y => y.Id == x)))
+			{
+				return Task.FromResult(PreconditionResult.FromSuccess());
+			}
+			else if (MatchAllRoles && _roleIds.All(x => user.Roles.Any(y => y.Id == x)))
+			{
+				return Task.FromResult(PreconditionResult.FromSuccess());
+			}
+			return Task.FromResult(PreconditionResult.FromError("User doesn't match any roles."));
+		}
+	}
+}
